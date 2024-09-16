4b0dd38e0aaf153086966f315ccfae07e3f82828
Artem Saakov <asaakov@umich.edu>
2024-09-16 11:06:31 -0400

    change some things to bracket notation

--- js/get_weather.js
+++ js/get_weather.js
@@ -1,32 +1,28 @@
 const displayData = (data) => {
-    document.querySelector("#location").innerHTML = `Weather in ${data.name}, ${data.sys.country}`;
-    document.getElementById("temp").innerHTML = `${data.main.temp}°F`;
-    document.getElementById("realfeel").innerHTML = `RealFeel: ${data.main.feels_like}°F`;
-    document.getElementById("weather_main").innerHTML = data.weather[0].main;
-    document.getElementById("weather_desc").innerHTML = data.weather[0].description;
-    (data.clouds.all <= 30) ? document.getElementById("clouds").innerHTML = "Sunny"
-        : (data.clouds.all <= 60) ? document.getElementById("clouds").innerHTML = "Mostly Sunny"
-        : (data.clouds.all <= 70) ? document.getElementById("clouds").innerHTML = "Partly Cloudy"
-        : (data.clouds.all <= 90) ? document.getElementById("clouds").innerHTML = "Mostly Cloudy"
-        : document.getElementById("clouds").innerHTML = "Cloudy";
+    document["querySelector"]("#location")["innerHTML"] = `Weather in ${data["name"]}, ${data["sys"]["country"]}`;
+    document["querySelector"]("#temp")["innerHTML"] = `${data["main"]["temp"]}°F`;
+    document["querySelector"]("#realfeel")["innerHTML"] = `RealFeel: ${data.main.feels_like}°F`;
+    document["querySelector"]("#weather_main")["innerHTML"] = data.weather[0].main;
+    document["querySelector"]("#weather_desc")["innerHTML"] = data.weather[0].description;
+    (data['clouds']['all'] <= 30) ? document['querySelector']("#clouds")['innerHTML'] = "Sunny"
+        : (data['clouds']['all'] <= 60) ? document['querySelector']("#clouds")['innerHTML'] = "Mostly Sunny"
+        : (data['clouds']['all'] <= 70) ? document['querySelector']("#clouds")['innerHTML'] = "Partly Cloudy"
+        : (data['clouds']['all'] <= 90) ? document['querySelector']("#clouds")['innerHTML'] = "Mostly Cloudy"
+        : document['querySelector']("#clouds")['innerHTML'] = "Cloudy";
 }
 
 // Fetches the weather data from the OpenWeatherMap API and returns the data as a JSON object
 // based on if city or coordinates are used as the search parameter.
 const accessAPI = async (lat = 0, lon = 0, city = "") => {
+    let response = '';
     if (lat !== 0 && lon !== 0) {
-        console.log("3. Search by lat/lon initiated");
         const initResponse = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=imperial&appid=ed1073051910972ddcd1959352a015d7`);
-        const response = await initResponse.json();
-        console.log(response);
+        response = await initResponse.json();
         return response;
     }
-    console.log("2. Search by city initiated");
     city = city.split(" ").join("%20");
-    console.log(city);
     const initResponse = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=ed1073051910972ddcd1959352a015d7`);
     response = await initResponse.json();
-    console.log(response);
     return response;
 }
 
@@ -35,29 +31,25 @@ const accessAPI = async (lat = 0, lon = 0, city = "") => {
 // and calls the accessAPI function with the obtained latitude and longitude.
 // If the user denies location access, an error message is displayed on the page.
 const currentLocationFetch = () => {
-    navigator.geolocation.getCurrentPosition(async function(coord) {
-        const lat = coord.coords.latitude;
-        const lon = coord.coords.longitude;
-        console.log(`2. lat: ${lat}, lon: ${lon}`);
-        const response = await accessAPI(lat, lon);
+    navigator['geolocation']["getCurrentPosition"](async (coord) => {
+        const {latitude, longitude} = coord["coords"];
+        const response = await accessAPI(latitude, longitude);
         displayData(response);
     },
         // Built in error handling for getCurrentPosition
         (error) => {
             if (error.code == error.PERMISSION_DENIED) {
-                console.log("2. Location access blocked");
-                document.getElementById("error").innerHTML = "Location access blocked :(";
+                document["querySelector"]("#error")["innerHTML"] = "Location access blocked :(";
             }
         });
 }
 
 // Event listener for the search button. Initiates either the currentLocationFetch
 // function or the accessAPI function depending on the input.
-document.getElementById("weather").addEventListener("submit", async function (event) {
-    event.preventDefault();
+document["querySelector"]("#weather")["addEventListener"]("submit", async (e) => {
+    e.preventDefault();
     // Grabs the input from the search bar
-    const location = document.getElementById("search_text").value;
-    console.log(`1. the location: ${location}`);
+    const location = document["querySelector"]("#search_text")["value"];
     // Checks if search input is blank and empty
     if (location.trim().length === 0) {
         currentLocationFetch();
